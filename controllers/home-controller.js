776702c8fd2a250a599d7d7b3a99367abcf2dab9
Ludovic Christmann <ludochristm@gmail.com>
2017-12-07 23:39:19 +0100

    Creating first views and front end architecture

---
+++ controllers/home-controller.js
@@ -0,0 +1,12 @@
+/*
+$(document).ready(function() {
+	$("#show-signup").on("click", function(){
+		alert("The signup was clicked.");
+	}); 
+
+	$("#show-signin").on("click", function(){
+		alert("The signin was clicked.");
+	}); 
+}); 
+*/
+
