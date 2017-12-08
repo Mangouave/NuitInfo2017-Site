1af7bfc6f033daf84e0c304b5fada7facc6d1d95
Vincent M <vincent.marceddu@gmail.com>
2017-12-08 02:48:18 +0100

    fixed index

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html>
-
-	<?php include "includes/head.php" ?>
-	<body>
-		<?php include "includes/header.php" ?>
-		<div id="event-jumbotron" class="jumbotron jumbotron-fluid">
-			<div class="container">
-				<h1 class="display-3 display-jumbotron">Tu prends l'auto, lâche ton mojito ! </h1>
-			</div>
-		</div>
-		
-		<?php include "includes/footer.php" ?>
-
-	</body>
-
-</html>
+<?php 
+	header('Location: views/home.php');
+?>
